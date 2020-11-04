c7e3a6546e1e38e9c369f5bb4d8066ac929e94ea
martinaasen <=>
2020-11-04 09:27:27 +0100

    Task 2

--- src/main/scala/Bank.scala
+++ src/main/scala/Bank.scala
@@ -1,15 +1,37 @@
+import java.lang.RuntimeException
+
 class Bank(val allowedAttempts: Integer = 3) {
 
     private val transactionsQueue: TransactionQueue = new TransactionQueue()
     private val processedTransactions: TransactionQueue = new TransactionQueue()
 
-    def addTransactionToQueue(from: Account, to: Account, amount: Double): Unit = ???
+    def addTransactionToQueue(from: Account, to: Account, amount: Double): Unit = {
+        Transaction trans = new Transaction(transactionsQueue, processedTransactions, from, to, amount, allowedAttempts)
+        transactionsQueue.push(trans)
+        new Thread() {
+            override def run() {
+                processTransactions
+            }
+        }.start();
+    }
                                                 // TODO
                                                 // project task 2
                                                 // create a new transaction object and put it in the queue
                                                 // spawn a thread that calls processTransactions
 
-    private def processTransactions: Unit = ???
+    private def processTransactions: Unit = {
+        Transaction trans = transactionsQueue.pop
+        new Thread(){
+            override def run(): Unit = {
+                if (trans.status == TransactionStatus.PENDING){
+                    transactionsQueue.push(trans)
+                    processTransactions
+                } else{
+                    processedTransactions.push(trans)
+                }
+            }.start()
+        }
+    }
                                                 // TOO
                                                 // project task 2
                                                 // Function that pops a transaction from the queue
